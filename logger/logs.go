c930ce7eaa1e66f197737d8b35b30780447274d8
张鑫 <850943653@qq.com>
2022-04-25 10:18:03 +0800

    重构

---
+++ logger/logs.go
@@ -0,0 +1,19 @@
+package logger
+
+import (
+	"github.com/sirupsen/logrus"
+)
+
+var Logs *logrus.Logger
+
+func init() {
+	Logs = NewLogger(&Config{
+		Debug:        true,
+		Name:         "./log/run.log",
+		Level:        5,
+		Caller:       true,
+		Color:        true,
+		MaxAge:       0,
+		RotationTime: 0,
+	})
+}
