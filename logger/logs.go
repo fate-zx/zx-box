348360590e526957e8c35794925cde3b6c6ef33d
‘zhangxin’ <850943653@qq.com>
2022-07-01 16:19:43 +0800

    1

--- logger/logs.go
+++ logger/logs.go
@@ -2,18 +2,19 @@ package logger
 
 import (
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 var Logs *logrus.Logger
 
 func init() {
 	Logs = NewLogger(&Config{
-		Debug: true,
-		//Name:         "./log/run.log",
+		Debug:        true,
+		Name:         "./log/run.log",
 		Level:        5,
 		Caller:       true,
 		Color:        true,
-		MaxAge:       0,
-		RotationTime: 0,
+		MaxAge:       10 * time.Second,
+		RotationTime: 10 * time.Second,
 	})
 }
